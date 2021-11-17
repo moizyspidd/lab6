ec15fb155cd4796abd99dd40777581088da00564
moizyspidd <gbelimenko@gmail.com>
2021-11-18 00:10:35 +0200

    Add test_data.cpp

---
+++ test_data.cpp
@@ -0,0 +1,19 @@
+#include "IntData.h"
+
+int main()
+{
+    IntData data1;
+    IntData data2(10);
+    IntData * pData1 = new IntData;
+    IntData * pData2 = new IntData(20);
+
+    data1.print();
+    data2.print();
+    pData1->print();
+    pData2->print();
+
+    delete pData1;
+    delete pData2;
+
+    return 0;
+}
