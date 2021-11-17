01147da7e4f579bd1a04f8c6d778ff1f1dcb0c1f
moizyspidd <gbelimenko@gmail.com>
2021-11-18 00:11:12 +0200

    Add intData.h

---
+++ IntData.h
@@ -0,0 +1,13 @@
+#ifndef __INTDATA_H__
+#define __INTDATA_H__
+
+#include "BaseData.h"
+class IntData : public BaseData {
+private:
+    int m_data;
+public:
+        IntData(int data=0);
+        ~IntData();
+        void print();
+};
+#endif // __INTDATA_H__
